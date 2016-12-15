3a5dcca70a45e64e757fc2a437a34061069dcee3
sufferpeach <sufferpeach@gmail.com>
2016-12-15 23:31:59 +0300

    sadassad

--- sig_cpy/sig_cpy.c
+++ sig_cpy/sig_cpy.c
@@ -4,11 +4,31 @@
 #include <dirent.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
 
+#define SIG_0 0
+#define SIG_1 1
+
+void doNothing (int signal);
+void handleSignal(int signal);
 void cpy(const char* from, const char* to);
 void fileCpy(const char* from, const char* to);
 char* findLastIteration(const char* str, const char symb);    //if there are no slashes in str - it returns NULL
 char* myStrcat(char* dest, const char* src);
+
+struct SWritingByte
+{
+    char byte;
+    size_t bit;
+    FILE* output;
+}
+typedef SWritingByte;
+
+SWritingByte writingByte;
+pid_t pid;
 
 int main(int argc, char** argv, char** envp)
 {
@@ -118,16 +138,110 @@ void cpy(const char* from, const char* to)
 
 void fileCpy(const char* from, const char* to)
 {
-    SBuff* buff = createBuff(BUFF_SIZE);
     FILE* input = fopen(from, "rt");
     FILE* output = fopen(to, "wt");
-    while (fgets(buff->data, buff->size, input))
+
+    if(!output)
     {
-        fprintf(output, "%s", buff->data);
+        perror("An error occurred while creating the file");
+        exit(1);
     }
+
+    writingByte.byte = 0;
+    writingByte.bit = 128;
+    writingByte.output = output;
+
+    char symbIn;
+    struct sigaction write;
+    struct sigaction getAns;
+    pid_t ppid;
+
+    switch (pid = fork())
+    {
+        case -1:
+            perror("An error occurred while creating a process");
+            exit(errno);
+        break;
+        case 0:
+            ppid = getppid();
+
+            memset(&getAns, 0, sizeof(getAns));
+            getAns.sa_handler = doNothing;
+            sigset_t ans;
+            sigemptyset(&ans);
+            sigaddset(&ans, SIG_0);
+            sigaddset(&ans, SIG_1);
+            getAns.sa_mask = ans;
+            sigaction(SIG_0, &getAns, 0);
+            sigaction(SIG_1, &getAns, 0);
+
+            while (read(input, &symbIn, 1))
+            {
+                printf("%c", symbIn);
+                for (int i = 128; i > 0; i /= 2)
+                {
+                    if (symbIn & i)
+                    {
+                        kill(ppid, SIG_1);
+                    }
+                    else
+                    {
+                        kill(ppid, SIG_0);
+                    }
+
+                    sigsuspend(&ans);
+                }
+            }
+            exit(0);
+        break;
+        default:
+            memset(&write, 0, sizeof(write));
+            write.sa_handler = handleSignal;
+            sigset_t signals;
+            sigemptyset(&signals);
+            sigaddset(&signals, SIG_0);
+            sigaddset(&signals, SIG_1);
+            write.sa_mask = signals;
+            sigaction(SIG_0, &write, 0);
+            sigaction(SIG_1, &write, 0);
+
+            waitpid(pid, NULL, 0);  
+        break;
+    }
+
     fclose(input);
     fclose(output);
-    destroyBuff(buff);
+}
+
+void doNothing (int signal)
+{
+    return;
+}
+
+void handleSignal(int signal)
+{
+    switch (signal)
+    {
+        case SIG_0:
+            writingByte.bit /= 2;
+            kill(pid, SIG_0);
+        break;
+        case SIG_1:
+            writingByte.byte += writingByte.bit;
+            writingByte.bit /= 2;
+            kill(pid, SIG_1);
+        break;
+        default:
+            perror("An error occured while handling signal");
+            exit(-1);
+        break;
+    }
+    
+    if (writingByte.bit == 0)
+    {
+        fprintf(writingByte.output,"%c", writingByte.byte);
+        writingByte.bit = 128;
+    }
 }
 
 char* myStrcat(char* dest, const char* src)
